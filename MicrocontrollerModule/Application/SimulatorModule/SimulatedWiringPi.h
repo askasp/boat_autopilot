053199f47e7f549e1713e67bdf7e8c419032f56f
ask.com <akselkjetsa@gmail.com>
2018-07-27 16:50:31 +0200

    Add simulatedwiringpi header file

---
+++ MicrocontrollerModule/Application/SimulatorModule/SimulatedWiringPi.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+void pinMode(int pin, int outin);
+void digitalWrite(int pin, int status);
+void wiringPiSetup();
+
+const int OUTPUT = 1;
+const int INPUT = 0;
+const int LOW = 0;
+const int HIGH = 1;
